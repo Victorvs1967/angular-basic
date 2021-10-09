e76d79410238b363ead78985f157f830b20fa4ec
Victors1967 <victorsmirnov@me.com>
2021-10-09 22:58:36 +0300

    angular-base-10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,36 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  user = {
-    username: '',
-    email: '',
-    secret: '',
-    questionAnswer: '',
-    gender: ''
-  };
-  submitted = false;
-
-  @ViewChild('f') signupForm: NgForm | undefined;
-  defaultQuestion = 'pet';
-  answer = '';
   genders = ['male', 'female'];
+  signupForm!: FormGroup;
 
   constructor() {}
 
   ngOnInit() {
-  }
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    this.signupForm?.form.patchValue({
-      username: suggestedName
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'gender': new FormControl('male'),
     });
-  }
 
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
-  // }
+    this.signupForm.setValue({
+      'username': 'Superuser',
+      'email': 'victor@mail.me',
+      'gender': 'male'
+    });
+
+  }
 
   onSubmit() {
-    this.submitted = true;
-    this.user.username = this.signupForm?.value.username;
-    this.user.email = this.signupForm?.value.email;
-    this.user.secret = this.signupForm?.value.secret;
-    this.user.questionAnswer = this.signupForm?.value.questionAnswer;
-    this.user.gender = this.signupForm?.value.gender;
-    this.signupForm?.reset();
+    console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
 }
